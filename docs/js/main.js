be0f297689f35b91b52d8fb005abbb5481123e10
Sk Jhohev <sjhoh@amazon.com>
2018-02-08 10:12:00 +0530

    Calling video.play() for event: touchmove

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,22 @@
 
+Object.defineProperty(HTMLMediaElement.prototype, 'isPlaying', {
+    get: function(){
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+})
+
 function userInteracted() {
     console.log("user interacted");
     var videos = document.querySelectorAll("video");
     videos.forEach(function (video) {
-        video.play();
+        try {
+            if(!video.isPlaying) video.play();
+        } catch {
+            video.play();
+        }
     });
 }
 
 document.body.addEventListener('click', userInteracted, true);
 window.addEventListener('scroll', userInteracted, true);
+window.addEventListener('touchmove', userInteracted, true);
