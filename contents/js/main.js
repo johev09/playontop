f03f479432c01168149256b5f536b988daa21440
Sk Jhohev <sjhoh@amazon.com>
2018-02-08 11:24:45 +0530

    Fixed main.js

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,19 +1,9 @@
 
-Object.defineProperty(HTMLMediaElement.prototype, 'isPlaying', {
-    get: function(){
-        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-    }
-})
-
 function userInteracted() {
     console.log("user interacted");
     var videos = document.querySelectorAll("video");
-    videos.forEach(function (video) {
-        try {
-            if(!video.isPlaying) video.play();
-        } catch {
-            video.play();
-        }
+    videos.forEach(function (video) {video.play();
+        video.play();
     });
 }
 
